47d49b60597fff4d554e66f5eef7396dc5764f73
wkcn <wkcn@live.cn>
2018-05-10 10:23:07 +0800

    fix Makefile, use C++11

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -2,13 +2,14 @@ import mxnet as mx
 import numpy as np
 import mobula
 
-mobula_op = mobula.load_lib('./mobula/build/mobula_op.so')
+def test_lib():
+    mobula_op = mobula.load_lib('./mobula/build/mobula_op.so')
 
-dtype = np.float32
-a = mx.nd.array([1,2,3], dtype = dtype)
-b = mx.nd.array([4,5,6], dtype = dtype)
-c = mx.nd.array([0,0,0], dtype = dtype)
+    dtype = np.float32
+    a = mx.nd.array([1,2,3], dtype = dtype)
+    b = mx.nd.array([4,5,6], dtype = dtype)
+    c = mx.nd.array([0,0,0], dtype = dtype)
 
-mobula_op.add(a.size, a, b, c) 
+    mobula_op.add(a.size, a, b, c)
 
-assert ((a + b).asnumpy() == c.asnumpy()).all()
+    assert ((a + b).asnumpy() == c.asnumpy()).all()
