fix precision in roi align test

tests/test_roi_align_op.py
@@ -2,29 +2,31 @@ import mxnet as mx
 import numpy as np
 import mobula_op
 
+T = np.float32
+
 def bilinear_interpolate(bottom, height, width, y, x):
     if y < -1.0 or y > height or x < -1.0 or x > width:
         return 0.0, []
-    x = max(0.0, x)
-    y = max(0.0, y)
+    x = T(max(0.0, x))
+    y = T(max(0.0, y))
     x_low = int(x)
     y_low = int(y)
     if x_low >= width - 1:
         x_low = x_high = width - 1
-        x = x_low
+        x = T(x_low)
     else:
         x_high = x_low + 1
 
     if y_low >= height - 1:
         y_low = y_high = height - 1
-        y = y_low
+        y = T(y_low)
     else:
         y_high = y_low + 1
 
-    ly = y - y_low
-    lx = x - x_low
-    hy = 1.0 - ly
-    hx = 1.0 - lx
+    ly = y - T(y_low)
+    lx = x - T(x_low)
+    hy = T(1.0) - ly
+    hx = T(1.0) - lx
 
     v1 = bottom[y_low, x_low]
     v2 = bottom[y_low, x_high]
@@ -54,6 +56,41 @@ def bilinear_interpolate(bottom, height, width, y, x):
            ]
     return val, grad
 
+def bilinear_interpolate_gradient(height, width, y, x):
+    '''
+    return:
+        w1, w2, w3, w4
+        x_low, x_high, y_low, y_high
+    '''
+    if y < -1.0 or y > height or x < -1.0 or x > width:
+        return 0,0,0,0,-1,-1,-1,-1
+    x = T(max(0.0, x))
+    y = T(max(0.0, y))
+    x_low = int(x)
+    y_low = int(y)
+    if x_low >= width - 1:
+        x_low = x_high = width - 1
+        x = T(x_low)
+    else:
+        x_high = x_low + 1
+
+    if y_low >= height - 1:
+        y_low = y_high = height - 1
+        y = T(y_low)
+    else:
+        y_high = y_low + 1
+
+    ly = y - T(y_low)
+    lx = x - T(x_low)
+    hy = T(1.0) - ly
+    hx = T(1.0) - lx
+
+    w1 = hy * hx
+    w2 = hy * lx
+    w3 = ly * hx
+    w4 = ly * lx
+    return w1, w2, w3, w4, x_low, x_high, y_low, y_high
+
 def roialign_forward_backward(data, rois, pooled_size, spatial_scale, sampling_ratio, dy):
     N, C, H, W = data.shape
     R = rois.shape[0]
@@ -68,33 +105,68 @@ def roialign_forward_backward(data, rois, pooled_size, spatial_scale, sampling_r
     for r in range(R):
         batch_ind = int(rois[r, 0])
         sw, sh, ew, eh = rois[r, 1:5] * spatial_scale
-        roi_w = max(ew - sw, 1.0)
-        roi_h = max(eh - sh, 1.0)
-        bin_h = roi_h * 1.0 / PH
-        bin_w = roi_w * 1.0 / PW
+        roi_w = T(max(ew - sw, 1.0))
+        roi_h = T(max(eh - sh, 1.0))
+        bin_h = roi_h / T(PH)
+        bin_w = roi_w / T(PW)
         bdata = data[batch_ind]
         if sampling_ratio > 0:
             roi_bin_grid_h = roi_bin_grid_w = sampling_ratio
         else:
             roi_bin_grid_h = int(np.ceil(roi_h * 1.0 / PH))
             roi_bin_grid_w = int(np.ceil(roi_w * 1.0 / PW))
-        count = roi_bin_grid_h * roi_bin_grid_w
+        count = T(roi_bin_grid_h * roi_bin_grid_w)
         for c in range(C):
             for ph in range(PH):
                 for pw in range(PW):
-                    val = 0.0
+                    val = T(0.0)
                     for iy in range(roi_bin_grid_h):
-                        y = sh + ph * bin_h + (iy + 0.5) * bin_h / roi_bin_grid_h
+                        y = sh + T(ph) * bin_h + T(iy + 0.5) * bin_h / T(roi_bin_grid_h)
                         for ix in range(roi_bin_grid_w):
-                            x = sw + pw * bin_w + (ix + 0.5) * bin_w / roi_bin_grid_w
+                            x = sw + T(pw) * bin_w + T(ix + 0.5) * bin_w / T(roi_bin_grid_w)
                             v, g = bilinear_interpolate(bdata[c], H, W, y, x)
                             val += v
                             # compute grad
                             for qy, qx, qw in g:
-                                dx[batch_ind, c, qy, qx] += dy[r, c, ph, pw] * qw * 1.0 / count
+                                dx[batch_ind, c, qy, qx] += dy[r, c, ph, pw] * qw / count
 
-                    out[r, c, ph, pw] = val * 1.0 / count
+                    out[r, c, ph, pw] = val / count
     return out, [dx, drois]
+
+def roialign_backward(bottom_diff, rois, pooled_size, spatial_scale, sampling_ratio, top_diff):
+    N, C, H, W = bottom_diff.shape
+    R = rois.shape[0]
+    PH, PW = pooled_size
+    assert len(rois.shape) == 2
+    assert rois.shape[1] == 5
+
+    for r in range(R):
+        batch_ind = int(rois[r, 0])
+        sw, sh, ew, eh = rois[r, 1:5] * T(spatial_scale)
+        roi_w = max(ew - sw, T(1.0))
+        roi_h = max(eh - sh, T(1.0))
+        bin_h = roi_h / T(PH)
+        bin_w = roi_w / T(PW)
+        if sampling_ratio > 0:
+            roi_bin_grid_h = roi_bin_grid_w = int(sampling_ratio)
+        else:
+            roi_bin_grid_h = int(np.ceil(roi_h * 1.0 / PH))
+            roi_bin_grid_w = int(np.ceil(roi_w * 1.0 / PW))
+        count = T(roi_bin_grid_h * roi_bin_grid_w)
+        for c in range(C):
+            for ph in range(PH):
+                for pw in range(PW):
+                    for iy in range(roi_bin_grid_h):
+                        y = sh + T(ph) * bin_h + T(iy + 0.5) * bin_h / T(roi_bin_grid_h)
+                        for ix in range(roi_bin_grid_w):
+                            x = sw + T(pw) * bin_w + T(ix + 0.5) * bin_w / T(roi_bin_grid_w)
+                            w1, w2, w3, w4, x_low, x_high, y_low, y_high = bilinear_interpolate_gradient(H, W, y, x)
+                            dtop = top_diff[r, c, ph, pw]
+                            if x_low >= 0 and x_high >= 0 and y_low >= 0 and y_high >= 0:
+                                bottom_diff[batch_ind, c, y_low, x_low] += dtop * w1 / count
+                                bottom_diff[batch_ind, c, y_low, x_high] += dtop * w2 / count
+                                bottom_diff[batch_ind, c, y_high, x_low] += dtop * w3 / count
+                                bottom_diff[batch_ind, c, y_high, x_high] += dtop * w4 / count
 
 def test_roi_align_sym():
     ctx = mx.cpu(0)
@@ -161,9 +233,14 @@ def test_roi_align_value():
     dy = mx.nd.random.uniform(-1, 1, (R, C) + pooled_size, dtype = dtype)
     output.backward(dy)
     real_output, [dx, drois] = roialign_forward_backward(data.asnumpy(), rois.asnumpy(), pooled_size, spatial_scale, sampling_ratio, dy.asnumpy())
+
+    bottom_diff = np.zeros(data.shape, dtype = T)
+    roialign_backward(bottom_diff, rois.asnumpy(), pooled_size, spatial_scale, sampling_ratio, dy.asnumpy())
+    assert (dx == bottom_diff).all(), np.max(np.abs(dx - bottom_diff))
+    assert np.allclose(data.grad.asnumpy(), bottom_diff), np.max(np.abs(data.grad.asnumpy() - bottom_diff))
+
     assert np.allclose(output.asnumpy(), real_output)
-    # It seems that the precision between Cfloat and Pyfloat is different.
-    assert np.allclose(data.grad.asnumpy(), dx, atol = 1e-6), np.abs(data.grad.asnumpy() - dx).max()
+    assert np.allclose(data.grad.asnumpy(), dx), np.max(np.abs(data.grad.asnumpy() - dx))
     assert np.allclose(rois.grad.asnumpy(), drois)
 
 if __name__ == '__main__':