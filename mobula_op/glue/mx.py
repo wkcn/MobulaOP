fix col2im, add conv, there is still computation error in conv op

mobula_op/glue/mx.py
@@ -12,7 +12,7 @@ def nd_iscontiguous(v):
     diffp = cp_end.value - cp.value
     return diffp == (v.size - 1) * 4
 
-mx.nd.empty_like = lambda x : mx.nd.empty(x.shape)
+mx.nd.empty_like = lambda x : mx.nd.empty(x.shape, dtype = np.float32)
 mx.nd.NDArray.iscontiguous = nd_iscontiguous
 
 def get_pointer(v):

mobula_op/maths.py
@@ -3,24 +3,27 @@ import operator
 from . import func
 from . import glue
 
+def is_same_shape(a, b):
+    return tuple(a) == tuple(b)
+
 def unary_op(f, a, out = None):
     if out is None:
         backend = glue.backend.get_var_backend(a)
         out = backend.F.empty_like(a, dtype = a.dtype)
     else:
-        assert out.shape == a.shape
+        assert is_same_shape(out.shape, a.shape)
     f(a.size, a, out)
     return out
 
 def binary_op(f, a, b, out = None):
     assert type(a) == type(b)
-    assert a.shape == b.shape
+    assert is_same_shape(a.shape, b.shape)
     # [TODO] broadcast
     if out is None:
         backend = glue.backend.get_var_backend(a)
         out = backend.F.empty_like(a, dtype = a.dtype)
     else:
-        assert out.shape == a.shape
+        assert is_same_shape(out.shape, a.shape)
     f(a.size, a, b, out)
     return out
 
@@ -32,6 +35,12 @@ mul = functools.partial(binary_op, func.mul)
 div = functools.partial(binary_op, func.div)
 
 def dot(a, b, out = None):
+    '''
+    dot(a, b)[i,j,k,m] = sum(a[i,j,:] * b[k,:,m])
+    numpy.dot and mxnet.nd.dot are different.
+    numpy.dot: dot(a, b)[i,j,k,m] = sum(a[i,j,:] * b[k,:,m])
+    mxnet.nd.dot: dot(x,y)[i,j,a,b] = sum(x[i,j,:]*y[:,a,b])
+    '''
     assert a.ndim >= 2 and b.ndim >= 2
     U = a.shape[-1]
     assert b.shape[-2] == U
@@ -44,7 +53,7 @@ def dot(a, b, out = None):
         backend = glue.backend.get_var_backend(a)
         out = backend.F.empty(out_shape, dtype = a.dtype)
     else:
-        assert out.shape == out_shape
+        assert is_same_shape(out.shape, out_shape)
     func.dot(a, b, I, U, K, M, out)
     return out
 
@@ -62,4 +71,54 @@ def transpose(data, axes, out = None):
     else:
         assert out.shape == out_shape
     func.transpose(data, data.shape, axes, out)
+    return out
+
+def tensordot(a, b, axes = 2, out = None):
+    if isinstance(axes, int):
+        axes = ([-axes], [axes])
+    assert len(axes) == 2
+    assert len(axes[0]) == len(axes[1])
+    def get_pos_axes(axes, ndim):
+        res = []
+        for x in axes:
+            if x < 0:
+                x += ndim
+                assert x >= 0
+            res.append(x)
+        return res
+
+    a_axes = get_pos_axes(axes[0], a.ndim)
+    b_axes = get_pos_axes(axes[1], b.ndim)
+
+    def get_rest_axes(axes, ndim):
+        return [i for i in range(ndim) if i not in axes]
+
+    a_rest_axes = get_rest_axes(a_axes, a.ndim)
+    b_rest_axes = get_rest_axes(b_axes, b.ndim)
+
+    def get_shape_i(axes, shape):
+        return [shape[x] for x in axes]
+
+    a_axes_shape = get_shape_i(a_axes, a.shape)
+    b_axes_shape = get_shape_i(b_axes, b.shape)
+
+    for ax, bx in zip(a_axes_shape, b_axes_shape):
+        assert ax == bx
+
+    a_rest_axes_shape = get_shape_i(a_rest_axes, a.shape)
+    b_rest_axes_shape = get_shape_i(b_rest_axes, b.shape)
+
+    prod = lambda x : functools.reduce(operator.mul, x)
+
+    a_transpose = transpose(a, a_rest_axes + a_axes).reshape((prod(a_rest_axes_shape), -1))
+    b_transpose = transpose(b, b_axes + b_rest_axes).reshape((-1, prod(b_rest_axes_shape)))
+
+    out_shape = a_rest_axes_shape + b_rest_axes_shape
+    if out is None:
+        backend = glue.backend.get_var_backend(a)
+        out = backend.F.empty(out_shape, dtype = a.dtype)
+    else:
+        assert is_same_shape(out.shape, out_shape)
+
+    dot(a_transpose, b_transpose, out = out.reshape(a_transpose.shape[0], b_transpose.shape[1]))
     return out