d0d1148b42e32577aab6a3c9522ca9efe31eae46
wkcn <wkcn@live.cn>
2018-09-12 10:32:58 +0800

    remove mutex in naive_ctx and use thread_local

--- mobula/inc/context/naive_ctx.h
+++ mobula/inc/context/naive_ctx.h
@@ -5,45 +5,39 @@
 #include <map>
 #include <utility>
 #include <thread>
-#include <mutex>
 
 namespace mobula {
 
 #if HOST_NUM_THREADS > 1
-// global_thread_id -> (local_thread_id, num_threads)
-static std::map<std::thread::id, std::pair<int, int> > MOBULA_KERNEL_INFOS;
-static std::mutex MOBULA_KERNEL_MUTEX;
+
+static thread_local int thread_local_i;
+static thread_local int thread_local_n;
+
+template<typename Func, typename ...Args>
+void thread_func_wrapper(Func func, const int i, const int nthreads, Args... args) {
+    thread_local_i = i;
+    thread_local_n = nthreads;
+    func(args...);
+}
 
 template<typename Func>
 class KernelRunner {
 public:
-    KernelRunner(Func func, int n):_func(func), _n(n){};
+    KernelRunner(Func func, int n):func_(func), n_(n){};
     template<typename ...Args>
     void operator()(Args... args){
-        const int nthreads = std::min(_n, HOST_NUM_THREADS);
+        const int nthreads = std::min(n_, HOST_NUM_THREADS);
         std::vector<std::thread> threads(nthreads);
-        std::vector<std::thread::id> thread_ids;
-        MOBULA_KERNEL_MUTEX.lock();
         for (int i = 0; i < nthreads; ++i) {
-            threads[i] = std::thread(_func, args...);
-            std::thread::id id = threads[i].get_id();
-            thread_ids.push_back(id);
-            MOBULA_KERNEL_INFOS[id] = std::make_pair(i, nthreads);
+            threads[i] = std::thread(thread_func_wrapper<Func, Args...>, func_, i, nthreads, args...);
         }
-        MOBULA_KERNEL_MUTEX.unlock();
         for (int i = 0;i < nthreads; ++i) {
             threads[i].join();
         }
-        // release resource
-        MOBULA_KERNEL_MUTEX.lock();
-        for (std::thread::id tid : thread_ids) {
-            MOBULA_KERNEL_INFOS.erase(tid);
-        }
-        MOBULA_KERNEL_MUTEX.unlock();
     }
 private:
-    Func _func;
-    int _n;
+    Func func_;
+    int n_;
 };
 
 inline MOBULA_DEVICE void get_parfor_range(const int n, const int num_threads, const int thread_id, int *start, int *end) {
@@ -63,13 +57,8 @@ inline MOBULA_DEVICE void get_parfor_range(const int n, const int num_threads, c
 
 template <typename Func>
 MOBULA_DEVICE void parfor(const int n, Func F) {
-    MOBULA_KERNEL_MUTEX.lock();
-    const std::pair<int, int> kernel_info = MOBULA_KERNEL_INFOS[std::this_thread::get_id()];
-    MOBULA_KERNEL_MUTEX.unlock();
-    const int thread_id = kernel_info.first;
-    const int num_threads = kernel_info.second;
     int start, end;
-    get_parfor_range(n, num_threads, thread_id, &start, &end);
+    get_parfor_range(n, thread_local_n, thread_local_i, &start, &end);
     for (int i = start; i < end; ++i) {
         F(i);
     }
