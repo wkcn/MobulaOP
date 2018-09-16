0fe2513a9fdc76a12a7625bbfcd33a977f6d77b5
Mercy <zhenglianmin96@163.com>
2018-09-16 11:41:07 -0700

    fix ROIAlign for pytorch

--- mobula/op/custom/ROIAlign/ROIAlign.py
+++ mobula/op/custom/ROIAlign/ROIAlign.py
@@ -2,6 +2,8 @@ import mobula
 from mobula.const import req
 import os
 
+import numpy as np
+
 @mobula.op.register
 class ROIAlign:
     def __init__(self, pooled_size, spatial_scale, sampling_ratio):
@@ -11,20 +13,25 @@ class ROIAlign:
     def forward(self, data, rois):
         if self.req[0] == req.null:
             return
+
         out = self.y
+        out_size = np.prod(out.size()) if callable(out.size) else out.size
+
         if self.req[0] == req.add:
             out_temp = self.F.empty_like(out)
-            mobula.func.roi_align_forward(out.size, data, self.spatial_scale, data.shape[1], data.shape[2], data.shape[3], self.pooled_size[0], self.pooled_size[1], self.sampling_ratio, rois, out_temp)
+            mobula.func.roi_align_forward(out_size, data, self.spatial_scale, data.shape[1], data.shape[2], data.shape[3], self.pooled_size[0], self.pooled_size[1], self.sampling_ratio, rois, out_temp)
             self.y[:] += out_temp
         else:
-            mobula.func.roi_align_forward(out.size, data, self.spatial_scale, data.shape[1], data.shape[2], data.shape[3], self.pooled_size[0], self.pooled_size[1], self.sampling_ratio, rois, self.y)
+            mobula.func.roi_align_forward(out_size, data, self.spatial_scale, data.shape[1], data.shape[2], data.shape[3], self.pooled_size[0], self.pooled_size[1], self.sampling_ratio, rois, self.y)
     def backward(self, dy):
         if self.req[0] == req.null:
             return
         if self.req[0] != req.add:
             self.dX[0][:] = 0
         data, rois = self.X
-        mobula.func.roi_align_backward(dy.size, dy, self.spatial_scale, data.shape[1], data.shape[2], data.shape[3], self.pooled_size[0], self.pooled_size[1], self.sampling_ratio, self.dX[0], rois)
+
+        dy_size = np.prod(dy.size()) if callable(dy.size) else dy.size
+        mobula.func.roi_align_backward(dy_size, dy, self.spatial_scale, data.shape[1], data.shape[2], data.shape[3], self.pooled_size[0], self.pooled_size[1], self.sampling_ratio, self.dX[0], rois)
         if self.req[1] not in [req.null, req.add]:
             self.dX[1][:] = 0
     def infer_shape(self, in_shape):
